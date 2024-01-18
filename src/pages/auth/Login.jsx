ccd6d20f6afbafb780b23aaccf2ff70d57620217
Rosy <rosydhakal333@gmail.com>
2024-01-18 19:06:11 +1100

    updated Login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,6 +6,7 @@ import Footer from '../../component/layout/Footer'
 import Header from '../../component/layout/Header'
 import CustomInput from '../../component/customInput/customInput';
 import Baselayout from '../../component/Baselayout'
+import { toast } from 'react-toastify';
 
 const inputs = [
   {name: "fname", label: "Username / Email", placeholder: "Enter your email/username.... ", type: "email", required: true },
@@ -14,14 +15,27 @@ const inputs = [
 ]
 
 const Login = () => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    const {name, value } = e.target;
+    setFormData({
+      ...formData, 
+      [name]:value});
+  }
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    toast("submitted", formData)
+
+  }
   return (
     <>
     <Baselayout>
     <div className = "p-3 border shadow rounded admin-form"> 
     <h1> Login </h1> 
-    <Form>
+    <Form onSubmit={handleSubmit}>
       {inputs.map(input => (
-          <CustomInput key= {input.name} label = {input.label} placeholder ={input.placeholder} {...input}/>
+          <CustomInput key= {input.name} label = {input.label} placeholder ={input.placeholder} {...input} onChange = {handleChange}/>
       ))}
       <Button variant="primary" type="submit">
           Login
