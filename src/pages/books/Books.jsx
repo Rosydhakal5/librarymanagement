f31bced5448b64ea726484e54c3562ab80643730
Rosy <rosydhakal333@gmail.com>
2024-01-25 22:06:44 +1100

    Book component updated

--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AdminLayout from '../../component/layout/AdminLayout'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 const Books = () => {
   return (
-    <div>Books</div>
+    <AdminLayout title={Books}>
+      <Link to={"/add"} className='d-flex justify-content-end'>
+        <Button>Add a book</Button></Link> 
+        {/* Book List in Table Format */}
+      </AdminLayout>
   )
 }
 
