39d2fcb0ffb49a77a7d61ae2cf59e7b3d3f20687
Rosy <rosydhakal333@gmail.com>
2024-01-29 20:52:41 +1100

    updated private route components

---
+++ src/component/privateRoute/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({children}) => {
+    const {userInfo} = useSelector(state => state.auth);
+  return (
+    userInfo.uid? <div>{children}</div> : <Navigate to = {"/login"} />
+
+  )
+}
+
+export default PrivateRoute
